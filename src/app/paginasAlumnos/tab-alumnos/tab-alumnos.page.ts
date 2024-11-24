343f5d679d4623dfc0b4c616598545cf6d4e0e72
FMA07 <fma07_92@hotmail.com>
2024-11-23 21:12:39 -0300

    Cambios a pdf y tabs, subido con comandos

--- src/app/paginasAlumnos/tab-alumnos/tab-alumnos.page.ts
+++ src/app/paginasAlumnos/tab-alumnos/tab-alumnos.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AnimationController } from '@ionic/angular';
 import { slideInAnimationDerAIzq } from 'src/app/animations/slide-in-animation/slide-in-animation.page';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-tab-alumnos',
@@ -9,13 +10,27 @@ import { slideInAnimationDerAIzq } from 'src/app/animations/slide-in-animation/s
 })
 export class TabAlumnosPage implements OnInit {
 
-  constructor(private animationCtrl: AnimationController) { }
+  nombreUsuario: string = '';
+
+  constructor(
+    private animationCtrl: AnimationController,
+    private authService: AuthService,
+  ) { }
 
   ngOnInit() {
+    this.mostrarNombreUsuario();
   }
 
   async ionViewDidEnter(){ //NO CAMBIAR NOMBRE ionViewDidEnter es un hook, no un método. Si lo cambias deja de funcionar la animación
     const animation = slideInAnimationDerAIzq(this.animationCtrl);
     await animation.play();
+  }
+
+  mostrarNombreUsuario(){
+    this.nombreUsuario = this.authService.getNombreUsuario();
+  }
+
+  cerrarSesion(){
+    this.authService.logout();
   }
 }
